b19d0e5c226ff6e0ffbeea0f9e73ae374dfca7e8
SanguineROSE <jijoazul@gmail.com>
2023-02-03 18:18:52 -0800

    Hero Class Interface

--- hero.java
+++ hero.java
@@ -1,5 +1,23 @@
-public class hero{
+public interface hero {
 
+    void getHealth();
 
+    void setHealth();
 
-}
+    void getPowerPoints();
+
+    void setPowerPoints();
+
+    void attack();
+
+    void specialAbility();
+
+    void getXP();
+
+    void moveLeft();
+
+    void moveRight();
+
+    void moveForward();
+
+    void moveBack()
